5e3b87bb248ac1fc82601889d7ec754d297a13bc
aparna <aparnac.wiz91@gmail.com>
2023-08-07 14:33:33 +0530

    add static file

--- freelancer/settings.py
+++ freelancer/settings.py
@@ -149,9 +149,14 @@ AUTH_USER_MODEL='account.UserAccount'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
+LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'),)
 
 
 SWAGGER_SETTINGS = {
